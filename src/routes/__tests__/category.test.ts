012ce8f05d7657843f917aca215da424c949170d
Michael Desanker <mdesanker@gmail.com>
2022-05-05 06:53:17 -0400

    Setup testing for category routes

---
+++ src/routes/__tests__/category.test.ts
@@ -0,0 +1,25 @@
+import app from "./app";
+import request from "supertest";
+import initializeTestServer from "../../utils/mongoTestConfig";
+import mongoose from "mongoose";
+
+// GLOBAL VARIABLES
+
+// TEST SETUP
+beforeAll(async () => {
+  await initializeTestServer();
+  // await seedDB();
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+// GET ROUTES
+describe("GET /category/all", () => {
+  it("return array of all product categories", async () => {
+    const res = await request(app).get("/category/all");
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
