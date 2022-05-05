b8d4ca3bf09b05176ec60fe6daf82db08b1d2ca9
Michael Desanker <mdesanker@gmail.com>
2022-05-04 20:13:53 -0400

    Setup middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [/\localhost/, /\mdesanker.github.io/],
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = (process.env.PORT as string) || (process.env.DEV_PORT as string);
 
