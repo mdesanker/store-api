e8f1cf2654f81839e4110bbe172cca994032d41d
Michael Desanker <mdesanker@gmail.com>
2022-05-10 06:40:40 -0400

    Add validation for login route

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -21,6 +21,26 @@ const registerUser = [
   },
 ];
 
+const loginUser = [
+  // Validate and sanitize input
+  check("email", "Email is required").trim().notEmpty().isEmail(),
+  check("password", "Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+
+  // Error handling
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    next();
+  },
+];
+
 export default {
   registerUser,
+  loginUser,
 };
